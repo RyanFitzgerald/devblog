67aaa4a5f577dc121edec65c74e2e425cc8c2072
Tristan Schlötel <git@tristanschlotel.de>
2018-07-23 20:43:34 +0200

    added gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const userConfig = require('./config');
+const userConfig = require("./config");
 
 module.exports = {
   pathPrefix: `/devblog`,
@@ -24,7 +24,7 @@ module.exports = {
             options: {
               maxWidth: 700,
               linkImagesToOriginal: false,
-              wrapperStyle: 'margin: 15px -30px !important'
+              wrapperStyle: "margin: 15px -30px !important",
             },
           },
           {
@@ -49,5 +49,17 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: userConfig.title,
+        short_name: userConfig.title,
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: userConfig.primaryColor,
+        display: "minimal-ui",
+        icon: "src/main.jpg", // This path is relative to the root of the site.
+      },
+    },
   ],
-}
+};
